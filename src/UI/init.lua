5c9210b535244c3116c4df4319e12259f5605b92
MuryScript <muryscripting@gmail.com>
2026-05-22 00:52:12 +0800

    Create init.lua

---
+++ src/UI/init.lua
@@ -0,0 +1,14 @@
+return {
+	Window = require(script.Window),
+	Tab = require(script.Tab),
+	Section = require(script.Section),
+	Notification = require(script.Notification),
+	Dialog = require(script.Dialog),
+	Components = {
+		Button = require(script.Components.Button),
+		Toggle = require(script.Components.Toggle),
+		Slider = require(script.Components.Slider),
+		Textbox = require(script.Components.Textbox),
+		Dropdown = require(script.Components.Dropdown)
+	}
+}
